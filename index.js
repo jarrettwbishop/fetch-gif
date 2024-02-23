863f68be3f120defbb4f772736f30396799989ec
Jarrett Bishop <jarrettwbishop@gmail.com>
2024-02-23 13:11:07 -0500

    Follow TOP lesson and fetch cat GIF

---
+++ index.js
@@ -0,0 +1,11 @@
+const img = document.querySelector("img");
+fetch(
+	"https://api.giphy.com/v1/gifs/translate?api_key=A828jYppPu0MIuSzAwKP7ZlMhjHelImp&s=cats",
+	{ mode: "cors" }
+)
+	.then((response) => {
+		return response.json();
+	})
+	.then((response) => {
+		img.src = response.data.images.original.url;
+	});
